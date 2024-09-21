93621186582a9dca6c96e14884fc9d44aad39609
HayeonLee88 <hayeon0808@swu.ac.kr>
2024-09-21 20:27:43 +0900

    ✨ feat : [Sub-Task2] Quantization-Aware Traning #7 - networks.py에 Quantizable model 추가. - QAT 학습 코드 및 Quantization model eval 코드 작성.

--- Sub-task2/Baseline_Model/networks.py
+++ Sub-task2/Baseline_Model/networks.py
@@ -27,8 +27,158 @@ SOFTWARE.
 Update: 2024.04.20.
 """
 
+from typing import Callable, Optional
+
+import torch
+from torch import Tensor
+
 import torch.nn as nn
+from torch.nn.quantized import FloatFunctional
+
 import torchvision.models as models
+
+
+def conv3x3(
+    in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1
+) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(
+        in_planes,
+        out_planes,
+        kernel_size=3,
+        stride=stride,
+        padding=dilation,
+        groups=groups,
+        bias=False,
+        dilation=dilation,
+    )
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class QuantizableBasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError("BasicBlock only supports groups=1 and base_width=64")
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+        # 'add()' or '+' must be replaced with 'FloatFunctional.add()' due to quantization
+        ###########################################
+        self.float_functional = FloatFunctional()
+        ###########################################
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        ###########################################
+        self.float_functional.add_relu(out, identity)
+        ###########################################
+
+        return out
+
+
+class QuantizableBottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition" https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.0)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.downsample = downsample
+        self.stride = stride
+
+        # 'add()' or '+' must be replaced with 'FloatFunctional.add()' due to quantization
+        ###########################################
+        self.float_functional = FloatFunctional()
+        ###########################################
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        ###########################################
+        out = self.float_functional.add_relu(out, identity)
+        ###########################################
+
+        return out
+
+
+models.resnet.BasicBlock = QuantizableBasicBlock
+models.resnet.Bottleneck = QuantizableBottleneck
 
 
 class ResExtractor(nn.Module):
@@ -46,18 +196,17 @@ class ResExtractor(nn.Module):
         super(ResExtractor, self).__init__()
 
         if resnetnum == "18":
-            self.resnet = models.resnet18(pretrained=pretrained)
+            resnet = models.resnet18(pretrained=pretrained)
         elif resnetnum == "34":
-            self.resnet = models.resnet34(pretrained=pretrained)
+            resnet = models.resnet34(pretrained=pretrained)
         elif resnetnum == "50":
-            self.resnet = models.resnet50(pretrained=pretrained)
+            resnet = models.resnet50(pretrained=pretrained)
         elif resnetnum == "101":
-            self.resnet = models.resnet101(pretrained=pretrained)
+            resnet = models.resnet101(pretrained=pretrained)
         elif resnetnum == "152":
-            self.resnet = models.resnet152(pretrained=pretrained)
+            resnet = models.resnet152(pretrained=pretrained)
 
-        self.modules_front = list(self.resnet.children())[:-2]
-        self.model_front = nn.Sequential(*self.modules_front)
+        self.model_front = nn.Sequential(*list(resnet.children())[:-2])
 
     def front(self, x):
         """In the resnet structure, input 'x' passes through conv layers except for fc layers."""
@@ -86,16 +235,19 @@ class MnExtractor(nn.Module):
 class Baseline_ResNet_color(nn.Module):
     """Classification network of color category based on ResNet18 structure."""
 
-    def __init__(self):
-        super(Baseline_ResNet_color, self).__init__()
+    def __init__(self, res_num="18"):
+        super().__init__()
 
-        self.encoder = ResExtractor("18")
+        self.encoder = ResExtractor(res_num)
         self.avg_pool = nn.AvgPool2d(kernel_size=7)
-        self.color_linear = nn.Linear(512, 19)
+        if res_num == "18":
+            self.color_linear = nn.Linear(512, 19)
+        elif res_num == "50":
+            self.color_linear = nn.Linear(2048, 19)
 
-    def forward(self, x):
+    def _forward_base(self, x):
         """Forward propagation with input 'x'."""
-        feat = self.encoder.front(x["image"])
+        feat = self.encoder.front(x)
         flatten = self.avg_pool(feat).squeeze()
 
         out = self.color_linear(flatten)
@@ -122,6 +274,54 @@ class Baseline_MNet_color(nn.Module):
         out = self.color_linear(flatten)
 
         return out
+
+
+class Quantizable_ResNet_color(Baseline_ResNet_color):
+    def __init__(self, res_num="18"):
+        super(Quantizable_ResNet_color, self).__init__(res_num=res_num)
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
+
+    def forward(self, x):
+        x = self.quant(x)
+        out = self._foward_implement(x)
+        out = self.dequant(out)
+        return out
+
+    def fuse_model(self) -> None:
+        for module_name, module in self.named_modules():
+            # torch.quantization.fuse_modules 사용시 convert to Quantization model의 weight가 갱신이 안됨.
+            if isinstance(module, models.resnet.BasicBlock):
+                # Fuse conv1, bn1, relu together
+                torch.ao.quantization.fuse_modules_qat(
+                    module, ["conv1", "bn1", "relu"], inplace=True
+                )
+                # Fuse conv2 and bn2 together
+                torch.ao.quantization.fuse_modules_qat(
+                    module, ["conv2", "bn2"], inplace=True
+                )
+                if module.downsample:
+                    torch.ao.quantization.fuse_modules_qat(
+                        module.downsample, ["0", "1"], inplace=True
+                    )
+
+            elif isinstance(module, models.resnet.Bottleneck):
+                # Fuse conv1, bn1, relu1 together
+                torch.ao.quantization.fuse_modules_qat(
+                    module, ["conv1", "bn1", "relu1"], inplace=True
+                )
+                # Fuse conv2 and bn2 and relu2 together
+                torch.ao.quantization.fuse_modules_qat(
+                    module, ["conv2", "bn2", "relu2"], inplace=True
+                )
+                # Fuse conv3, bn3 together
+                torch.ao.quantization.fuse_modules_qat(
+                    module, ["conv3", "bn3"], inplace=True
+                )
+                if module.downsample:
+                    torch.ao.quantization.fuse_modules_qat(
+                        module.downsample, ["0", "1"], inplace=True
+                    )
 
 
 if __name__ == "__main__":
